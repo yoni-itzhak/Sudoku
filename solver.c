15902ffc4ac3355365f2f72ea8d992be268ac74e
Yoni Itzhak <yoniitz@gmail.com>
2019-08-25 15:59:06 +0300

    removing comments

--- solver.c
+++ solver.c
@@ -7,27 +7,6 @@
 #include "game.h"
 #include "stack.h"
 
-
-
-/* Copyright 2019, Gurobi Optimization, LLC */
-/*
-  Sudoku example.
-
-  The Sudoku board is a 9x9 grid, which is further divided into a 3x3 grid
-  of 3x3 grids.  Each cell in the grid must take a value from 0 to 9.
-  No two grid cells in the same row, column, or 3x3 subgrid may take the
-  same value.
-
-  In the MIP formulation, binary variables x[i,j,v] indicate whether
-  cell <i,j> takes value 'v'.  The constraints are as follows:
-    1. Each cell must take exactly one value (sum_v x[i,j,v] = 1)
-    2. Each value is used exactly once per row (sum_i x[i,j,v] = 1)
-    3. Each value is used exactly once per column (sum_j x[i,j,v] = 1)
-    4. Each value is used exactly once per 3x3 subgrid (sum_grid x[i,j,v] = 1)
-
-  Input datasets for this example can be found in examples/data/sudoku*.
-*/
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
